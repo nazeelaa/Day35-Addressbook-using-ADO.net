d82268aa68b60319b0a24a499f9e88bd4d93c0df
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-12-14 18:58:09 +0530

    Address book program using MSSQL

---
+++ AddressBookUsingMSSQL/AddressBookModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookUsingMSSQL
+{
+    public class AddressBookModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zipcode { get; set; }
+        public Int64 Phone_no { get; set; }
+        public string Email { get; set; }
+      
+
+    }
+}
